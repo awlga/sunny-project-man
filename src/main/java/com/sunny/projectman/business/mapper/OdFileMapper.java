18949c61a0fb687198246bf9698f3d5eebc30648
wlg828@yeah.net <wlg828@yeah.net>
2019-12-30 14:38:06 +0800

    xq

---
+++ src/main/java/com/sunny/projectman/business/mapper/OdFileMapper.java
@@ -0,0 +1,10 @@
+package com.sunny.projectman.business.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sunny.projectman.business.entity.OdFile;
+
+/**
+ * 文件的mapper接口
+ */
+public interface OdFileMapper extends BaseMapper<OdFile> {
+}
